d99b1986add310c85e55ffce4464d1a52a815541
nkihbk <kenchasonakai@gmail.com>
2020-11-01 13:40:25 +0900

    Fix: 修正内容 _blankで開いた別タブを取得するように修正しました

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -16,8 +16,11 @@ RSpec.describe 'Task', type: :system do
 
       it 'Project詳細からTask一覧ページにアクセスした場合、Taskが表示されること' do
         # FIXME: テストが失敗するので修正してください
+        visit project_task_path(project, task)
         visit project_path(project)
         click_link 'View Todos'
+        windows = page.driver.browser.window_handles
+        page.driver.browser.switch_to.window(windows.last)
         expect(page).to have_content task.title
         expect(Task.count).to eq 1
         expect(current_path).to eq project_tasks_path(project)
